0c874053315e483ebd43becc677a15932dd2490e
Matheus Sena <matheus.sena@renova.net.br>
2025-08-13 10:25:46 -0300

    feat: Adiciona o diretório Metodos ao diretório principal contendo um projeto voltado para operações bancárias para exemplificar o uso de métodos em classes e objetos

--- ClassesEObjetos/Program.cs
+++ ClassesEObjetos/Program.cs
@@ -8,7 +8,22 @@ class DadosPessoa
     public string? sexo;
 }
 
-DadosPessoa primeiraPessoa = new DadosPessoa(); //Instanciação de um objeto da classe DadosPessoa
-primeiraPessoa.nome = "João"; //Atribuição de valor ao atributo nome
-primeiraPessoa.idade = 30; //Atribuição de valor ao atributo idade
-primeiraPessoa.sexo = "Masculino"; //Atribuição de valor ao atributo sexo
+class Program
+{
+    static void Main()
+    {
+        DadosPessoa primeiraPessoa = new(); //Instanciação de um objeto da classe DadosPessoa
+        primeiraPessoa.nome = "João"; //Atribuição de valor ao atributo nome
+        primeiraPessoa.idade = 30; //Atribuição de valor ao atributo idade
+        primeiraPessoa.sexo = "Masculino"; //Atribuição de valor ao atributo sexo
+
+        DadosPessoa dadosPessoa = new();
+        dadosPessoa.nome = "Maria"; //Atribuição de valor ao atributo nome
+        dadosPessoa.idade = 25; //Atribuição de valor ao atributo idade
+        dadosPessoa.sexo = "Feminino"; //Atribuição de valor ao atributo sexo
+    }
+}
+
+
+
+
---
+++ Metodos/Program.cs
@@ -0,0 +1,112 @@
+﻿//Declaração de Métodos em uma clase
+
+CadastroBancario cliente = new CadastroBancario();
+cliente.inicializacaoDoServico();
+
+
+class CadastroBancario
+{
+    string? nomeDoTitular;
+    double saldo;
+    string? numeroDaConta;
+    string? agencia;
+    char? opcao;
+
+    public void inicializacaoDoServico()
+    {
+        do
+        {
+            opcaoDoUsuario();
+            painelDeControle();
+        } while (true);
+    }
+    public void opcaoDoUsuario()    //O modificador "public" permite que o método seja acessado de fora da classe.
+    {
+        Console.WriteLine("_________________________________________________________");
+        Console.WriteLine("| 1 - Cadastrar cliente|   4 -         Sacar            |");
+        Console.WriteLine("| 2 - Consultar saldo  | 5 - Consultar dados do cliente |");
+        Console.WriteLine("| 3 -   Depositar      | 6 -           Sair             |");
+        Console.WriteLine("_________________________________________________________");
+        Console.Write("                 Digite a opção desejada: ");
+        opcao = Convert.ToChar(Console.ReadLine());
+    }
+
+    public void painelDeControle()
+    {
+        switch (opcao)
+        {
+            case '1':
+                cadastrarCliente();
+                break;
+            case '2':
+                consultarSaldo();
+                break;
+            case '3':
+                depositar();
+                break;
+            case '4':
+                sacar();
+                break;
+            case '5':
+                consultarDadosDaConta();
+                break;
+            case '6':
+                sair();
+                break;
+            default:
+                Console.WriteLine("Nenhuma das opções são válidas");
+                break;
+        }
+    }
+
+    public void cadastrarCliente()
+    {
+        Console.Write("\nDigite o nome do titular: ");
+        nomeDoTitular = Console.ReadLine();
+        saldo = 0.0;
+        Random rnd = new Random();
+        numeroDaConta = Convert.ToString(rnd.Next(00001, 99999));
+        agencia = Convert.ToString(rnd.Next(0001, 9999));
+    }
+
+    public void depositar()
+    {
+        Console.Write("Digite o valor do depósito: ");
+        double valorDeposito = Convert.ToDouble(Console.ReadLine());
+        if(valorDeposito > 0)
+        {
+            Console.WriteLine("Não foi possível realizar o depósito. Valor não compatível");
+        }
+        saldo += valorDeposito;
+    }
+
+    public void sacar()
+    {
+        Console.Write("Digite o valor de retirada : ");
+        double valorRetirada = Convert.ToDouble(Console.ReadLine());
+        if(valorRetirada > saldo)
+        {
+            Console.WriteLine("Não foi possível completar a operação. Saldo indisponível");
+        }else if(valorRetirada < 0)
+        {
+            Console.WriteLine("Não foi possível completar a operação. Operação inválida");
+        }
+        saldo -= valorRetirada;
+    }
+
+    public void consultarSaldo()
+    {
+        Console.WriteLine($"Saldo Atual : {saldo}");
+    }
+    public void consultarDadosDaConta()
+    {
+        Console.WriteLine($"Nome do titular : {nomeDoTitular}");
+        Console.WriteLine($"Número da conta : {numeroDaConta}");
+        Console.WriteLine($"Número da agência : {agencia}");
+    }
+
+    public void sair()
+    {
+        Environment.Exit(0);
+    }
+}
