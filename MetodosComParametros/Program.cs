27908064ece45fdf752867e93c79ed228967b734
Matheus Sena <matheussena02@gmail.com>
2025-08-14 13:39:12 -0300

    feat: Adiciona o diretório MetodosComParametros ao diretório principal da branch contendo a demonstração da criação e uso de métodos com parâmetros

---
+++ MetodosComParametros/Program.cs
@@ -0,0 +1,23 @@
+﻿//Declaração de Métodos que utilizam parâmetros 
+
+MinhaClasse usuario = new MinhaClasse();
+usuario.Saudacao("Matheus", DateTime.Now.ToString()); //Método "estático"
+
+//Método "dinâmico"
+Console.Write("Digite seu nome : ");
+string nome = Console.ReadLine();
+string data = DateTime.Now.ToShortDateString();
+usuario.Saudacao(nome, data);
+//Se passa os parâmetros no momento em que se chama o método desejado
+
+Console.ReadLine(); 
+public class MinhaClasse
+{
+    //Declara-se os parâmetros com o tipo de dado + o nome do parâmetro
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine($"Olá, {nome}");
+        Console.WriteLine($"Você está acessando no dia {data}");
+    }
+}
+
