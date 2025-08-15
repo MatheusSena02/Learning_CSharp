60cae56acf7fe36eddd79168bf36fbca06f6461d
Matheus Sena <matheussena02@gmail.com>
2025-08-15 10:52:58 -0300

    feat: Adiciona o diretório MetodosComRetorno ao diretório principal contendo arquivos demonstrativo do uso de métodos que possuem e usam retorno

---
+++ MetodosComRetorno/MetodosComRetorno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MetodosComRetorno
+{
+    public class Calculadora()
+    {
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+        public int Produto(int x, int y)
+        {
+            return x * y;
+        }
+        public int Quociente(int x, int y)
+        {
+            return x / y;
+        }
+        public int Modul(int x, int y)
+        {
+            return x % y;
+        }
+
+
+
+
+    }
+}
--- MetodosComRetorno/Program.cs
+++ MetodosComRetorno/Program.cs
@@ -1,1 +1,12 @@
 ﻿//Demonstração de Métodos com Retorno
+using MetodosComRetorno;
+
+var cal = new Calculadora();
+
+int valorUm = cal.Somar(10, 10);
+Console.WriteLine($"Valor Um : {valorUm}");
+valorUm = cal.Subtrair(10, 5);
+Console.WriteLine($"Valor Um : {valorUm}");
+Console.WriteLine(cal.Produto(2, 5));
+Console.WriteLine(cal.Quociente(2, 5));
+Console.ReadLine();
