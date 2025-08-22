0a752cf16f351b284961a08d8bc9bfb4d8e9cc1a
Matheus Sena <matheussena02@gmail.com>
2025-08-22 10:50:04 -0300

    feat: Adiciona o projeto ClasseArray à solução do projeto. Basicamente mostrando o caso de uso de métodos da classe array como Sort, BinarySearch e Reverse

---
+++ ClasseArray/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] nomes = { "Ana", "Maria", "Sérgio", "Pedro" };
+Console.WriteLine("Array original... ");
+
+foreach(string s in nomes)
+{
+    Console.WriteLine(s);
+}
+
+Array.Reverse(nomes);
+
+Console.WriteLine("Ordem invertida...");
+
+
+Console.WriteLine();
+
+foreach (string s in nomes)
+{
+    Console.WriteLine(s);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Ordenado...");
+Array.Sort(nomes);
+
+foreach (string s in nomes)
+{
+    Console.WriteLine(s);
+}
+
+Console.WriteLine("Localizando itens no array...");
+
+var indice = Array.BinarySearch(nomes, "Ana");
+
+Console.WriteLine("Ana está nessa posiçao na lista : " + indice);
+Console.Read();
