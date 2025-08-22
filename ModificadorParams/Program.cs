c8811842524a6445dae0c9b0d3f8c4b6c1d87744
Matheus Sena <matheussena02@gmail.com>
2025-08-22 12:42:42 -0300

    feat: Adiciona o projeto ModificadorParams à solução do projeto com demonstração de uso de passagem de argumentos usando arrays

---
+++ ModificadorParams/Program.cs
@@ -0,0 +1,35 @@
+﻿//É possível passar arrays como parâmetros para métodos, quando se deseja trabalhar com uma grande quantidade de parâmetros por exemplo
+
+int[] valores = {1, 2, 3, 4, 5};
+
+var resultado = Calcular.Soma(valores);
+Console.WriteLine($"Método sem params : {resultado}");
+
+resultado = Calcular.Soma(1, 4, 5 ,6 ,8); //O método params permite que passemos uma lista de valores
+Console.WriteLine($"Método com params : {resultado}");
+
+Console.Read(); 
+
+public class Calcular
+{
+    public static int Soma(int[] numeros)
+    {
+        int total = 0;
+        foreach(int i in numeros)
+        {
+            total += i;
+        }
+        return total;
+    }
+
+    public static int Soma(params double[] numeros)
+    {
+        //O método params permite que se passe um lista de valores no argumento, com formatação semelhante à de uma array
+        int total = 0;
+        foreach (int i in numeros)
+        {
+            total += i;
+        }
+        return total;
+    }
+}
