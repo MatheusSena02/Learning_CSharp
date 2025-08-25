db96980bf631e613c54b761ff34aa8c0eb879592
Matheus Sena <matheussena02@gmail.com>
2025-08-25 07:47:56 -0300

    feat: Adiciona o projeto ExemploPraticoII à solução do projeto contendo a resolução do exercícios proposta durante as aulas para arrays multidimensionais

---
+++ ExemploPraticoII/Program.cs
@@ -0,0 +1,18 @@
+﻿string[,] alunos = new string[2, 5];
+
+for(int i = 0; i < alunos.GetLength(0); i++)
+{
+    for(int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.Write($"Informe o nome do aluno da linha {i} e coluna {j} : ");
+        alunos[i, j] = Console.ReadLine();
+    }
+}
+
+foreach(var nome in alunos)
+{
+    Console.Write($"{nome}, ");
+}
+
+
+Console.ReadLine();
