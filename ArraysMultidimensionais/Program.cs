ae75c3acf41cfb31be3daddabfe8fb10e3df4c34
Matheus Sena <matheussena02@gmail.com>
2025-08-25 07:39:50 -0300

    feat: Adiciona o projeto ArraysMultidimensionais à solução do projeto, com os arquivos demonstrativos acerca da declaração e uso de matrizes bidimensionais

---
+++ ArraysMultidimensionais/Program.cs
@@ -0,0 +1,32 @@
+﻿//Declaração de um array bidimensional 
+
+int[,] array1 = new int[2, 3]; //Um array bidimensional com 2 linhas e 3 colunas
+//array1 é um array bidimensional com 2 elementos e cada elemento é um array unidimensional com 3 elementos
+
+for (int i = 0; i < array1.GetLength(0); i++) //GetLength(0) retorna o número de linhas do array (Primeira dimensão)
+{
+    for(int j = 0; j < array1.GetLength(1); j++) //GetLength(1) retorna o número de colunas do array (Segunda dimensão)
+    {
+        array1[i, j] = 1;
+    }
+}
+
+foreach(int n in array1)
+{
+    Console.Write("|" + n + "|" + "\t\n");
+}
+
+
+string[,] nomeProfissao = new string[4, 2] {
+    {"Ana", "Desenvolvedora"},
+    {"Anderson", "DBA"},
+    {"Carlos", "Gerente de Projetos"},
+    {"Daniel", "Desenvolvedor"}
+};
+
+foreach(string n in nomeProfissao) 
+{
+    Console.Write($"|{n}|");
+
+}
+Console.Read();
