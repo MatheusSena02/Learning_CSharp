7ac46fce7bf5c4947d50ed5e0817481f1d4a65da
Matheus Sena <matheussena02@gmail.com>
2025-09-02 09:04:46 -0300

    feat: Adiciona a solução dos exercícios proposta ao projeto Heranca

---
+++ ExercicioHeranca/Banco.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioHeranca
+{
+    public class Conta
+    {
+        protected int NumeroConta { get; set; }
+        protected string? NomeCliente { get; set; }
+
+        protected virtual decimal Saldo { get; set; } = 0.0m;
+
+        public Conta(string nome)
+        {
+            NomeCliente = nome;
+            Random seed = new Random();
+            NumeroConta = seed.Next(0000000001, 999999999);
+        }
+
+        public virtual void Depositar(decimal entrada)
+        {
+            if(entrada <= 0)
+            {
+                Console.Write("Valor digitado não válido");
+            }
+            else
+            {
+                Saldo += entrada;
+                Console.WriteLine("Depósito concluído com sucesso!");
+            }
+        }
+
+        public virtual void Sacar(decimal retirada)
+        {
+            if(retirada <= 0)
+            {
+                Console.WriteLine("Valor digitado não válido");
+            }else if(retirada > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+            else
+            {
+                Saldo -= retirada;
+                Console.WriteLine("\nSaque concluído com sucesso!");
+            }
+        }
+
+        public virtual void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo atual: {Saldo}");
+        }
+    }
+
+    public class Poupanca : Conta
+    {
+        protected decimal saldo = 0.0m;
+
+        public Poupanca(string nome) : base(nome)
+        {
+        }
+
+        internal decimal valorJuros = 0.005m;
+
+
+        protected override decimal Saldo
+        {
+            get { return saldo; }
+            set
+            {
+                if(saldo < 0)
+                {
+                    saldo = 0;
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+        public override void Depositar(decimal valor)
+        {
+            Saldo += valor + (valorJuros * valor);
+            Console.WriteLine("\nDepósito concluído com sucesso!");
+        }
+    }
+
+    public class Investimentos : Conta
+    {
+        private decimal saldo = 0.0m;
+        protected override decimal Saldo
+        {
+            get { return saldo; }
+            set
+            {
+                if (saldo < 0)
+                {
+                    saldo = 0;
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+        internal decimal valorJuros = 0.009m;
+        internal decimal valorImposto = 0.001m;
+
+        public Investimentos(string nome) : base(nome)
+        {
+
+        }
+
+        public override void Depositar(decimal valor)
+        {
+            saldo += valor + (valor*valorJuros);
+            Console.WriteLine("\nDepósito concluído com sucesso!");
+        }
+
+        public new void Sacar(decimal valor)
+        {
+            saldo -= valor + (valor * valorImposto);
+            Console.WriteLine("\nSaque concluído com sucesso!");
+        }
+    }
+
+}
--- ExercicioHeranca/Program.cs
+++ ExercicioHeranca/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExercicioHeranca;
+
+//Conta cliente1 = new("Matheus");
+//cliente1.ExibirSaldo();
+//cliente1.Depositar(250.50m);
+//cliente1.ExibirSaldo();
+//cliente1.Sacar(50.25m);
+//cliente1.ExibirSaldo();
+
+//Investimentos cliente2 = new ("Miranda");
+//cliente2.ExibirSaldo();
+//cliente2.Depositar(250.50m);
+//cliente2.ExibirSaldo();
+//cliente2.Sacar(50.25m);
+//cliente2.ExibirSaldo();
+
+Investimentos cliente3 = new ("Marcos");
+cliente3.ExibirSaldo();
+cliente3.Depositar(250.50m);
+cliente3.ExibirSaldo();
+cliente3.Sacar(50.25m);
+cliente3.ExibirSaldo();
+
+Console.Read();
