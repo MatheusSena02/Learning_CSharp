5b17c9fc4893614003f716145a95f39c9a1fefad
Matheus Sena <matheussena02@gmail.com>
2025-09-03 08:17:05 -0300

    feat: Implementa os arquivos de código demonstrativo acerca do uso de interfaces ao projeto Heranca

---
+++ Interfaces/Interface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IControle
+    {
+        void Desenhar();
+
+        public void Exibir()
+        {
+            Console.WriteLine("Teste...");
+        }
+    }
+
+  
+
+    public class Teste : IControle
+    {
+        public void Desenhar()
+        {
+            Console.WriteLine("Desenhando...");
+        }
+
+        public void Exibir() { }
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Interfaces;
+
+Teste primeiraInterface = new();
+
+primeiraInterface.Desenhar();
+primeiraInterface.Exibir();
+
+Console.Read();
