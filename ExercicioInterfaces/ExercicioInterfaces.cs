8c791898c355dd870dffcb501d576ad3ecac32ba
Matheus Sena <matheussena02@gmail.com>
2025-09-03 08:55:24 -0300

    feat: Adiciona o projeto ExercicioInterfaces à solução do projeto com os arquivos que solucionam os exercícios propostos

---
+++ ExercicioInterfaces/ExercicioInterfaces.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioInterfaces
+{
+    interface ISave
+    {
+        void Salvar();
+
+        void Compactar()
+        {
+            Console.WriteLine($"Arquivo compactado com sucesso");
+        }
+    }
+
+    public abstract class ArquivoBase
+    {
+        public virtual void Nome(string nome) { }
+    }
+
+    public class SalvarXml : ArquivoBase, ISave
+    {
+        private string? NomeArquivo {  get; set; }
+
+        public override void Nome(string nome)
+        {
+            NomeArquivo = nome;
+        }
+
+        public void Salvar()
+        {
+            Console.WriteLine($"Arquivo {NomeArquivo}.xml salvo com sucesso");
+        }
+    }
+
+    public class SalvarJson : ArquivoBase, ISave
+    {
+        private string? NomeArquivo { get; set; }
+
+        public override void Nome(string nome)
+        {
+            NomeArquivo = nome;
+        }
+        public void Salvar()
+        {
+            Console.WriteLine($"Arquivo {NomeArquivo}.json salvo com sucesso");
+        }
+    }
+
+}
---
+++ ExercicioInterfaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExercicioInterfaces;
+
+SalvarJson arquivoJson = new();
+arquivoJson.Nome("ENF_livro");
+arquivoJson.Salvar();
+// Converte a instância para a interface ISave para acessar o método Compactar
+((ISave)arquivoJson).Compactar();
+
+Console.WriteLine("\n");
+
+SalvarXml arquivoXml = new();
+arquivoXml.Nome("recibo");
+arquivoXml.Salvar();
+((ISave)arquivoXml).Compactar();
+
+
+Console.Read();
