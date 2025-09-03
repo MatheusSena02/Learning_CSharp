6756991de9c61a13af93e3e22d6c542e5b12584d
Matheus Sena <matheussena02@gmail.com>
2025-09-03 10:46:27 -0300

    feat: Adiciona o projeto Polimorfismo à solução do projeto com os arquivos em código demonstrando o uso do polimorfismo

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Polimorfismo;
+
+var figuras = new List<Figura>
+{
+    new Triangulo(),
+    new Circulo()
+};
+
+Figura figura1 = new Circulo(); // "crie um círculo a partir da classe Circulo e guarde ele dentro de uma variável que é vista como uma Figura”.
+//Cria uma instância do tipo da classe base que tem acesso às informãções de figura
+figura1.Desenhar(); //Executa o método Desenhar da classe Circulo
+
+Figura figura2 = new Triangulo();
+
+foreach(var figura in figuras)
+{
+    figura.Desenhar();
+}
+
+Console.Read();
---
+++ Polimorfismo/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class Triangulo : Figura
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um triângulo");
+            base.Desenhar();
+            //Executa o método da classe base
+        }
+    }
+}
