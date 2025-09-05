d4a9a5365cb26757b2af83b6fc5646454542180f
Matheus Sena <matheussena02@gmail.com>
2025-09-05 10:11:59 -0300

    feat: Incrementa o projeto Dictionary com arquivos de código contendo a demonstração do uso dos métodos e propriedades da classe Dictionary

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary <int, int> dict1 = new Dictionary<int, int>();
+//Key = int, Value = int
+
+var dict2 = new Dictionary<int, int>();
+
+dict1.Add(1, 2);
+dict2.Add(2, 3);
+dict1.Add(3, 4);
+
+var dict3 = new Dictionary<int, int>()
+{
+    {1, 500},
+    {5, 1000},
+    {2, 500},
+    {3, 1000}
+};
+
+try
+{
+    dict3.Add(3, 33);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Chave já utilizada");
+}
+
+var resultado = dict3.TryAdd(3, 300);
+Console.WriteLine($"O elemento foi incluído ? {resultado}");
+
+if (!dict3.ContainsKey(3))  //Averigua se a chave 3 está sendo utilizada no Dictionary
+{
+    dict3.Add(3, 850);
+    Console.WriteLine($"Elemento incluido: {dict3[3]}");
+}
+else
+{
+    Console.WriteLine("Chave já utilizada");
+}
+
+var dic3Ordenado = dict3.OrderBy(x => x.Key); //Ordena o dicionário dict3 a partir das chaves do dicionário
+foreach(var item in dic3Ordenado)
+{
+    Console.WriteLine($"{item.Key}.`{item.Value}");
+}
+Console.Read();
