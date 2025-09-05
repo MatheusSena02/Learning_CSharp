63367f361d32ae7bc57e16e448a63c8de94a47ee
Matheus Sena <matheussena02@gmail.com>
2025-09-05 09:08:18 -0300

    feat: Adiciona o projeto GetHashCodeEquals à solução do projeto com os arquivos contendo código que demonstram ocmo utilizar e a sintaxe dos método Equals e GetHashCode

---
+++ GetHashCodeEquals/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GetHashCode
+{
+    public class Class1
+    {
+        public bool Comparar <T> (T valor1, T valor2)
+        {
+            return valor1.Equals(valor2);
+        }
+    }
+
+    public class  Pessoa
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+
+        public Pessoa(int id, string? nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        //Sobrescrita do método Equals
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) return false;
+
+            if(obj is not Pessoa) return false;
+            //Se o parâmetro for diferente do um objeto da classe Pessoa, retorna false
+
+            var other = (Pessoa)obj; //Faz o boxing do obj, convertendo em um objeto do tipo Pessoa
+
+            return Id.Equals(other.Id);     //Faz a comparação baseado no atributo ID
+            //Retorna o resultado a operação do atributo Id do objeto com o Id do objeto Pessoa passado no argumento
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ GetHashCodeEquals/Program.cs
@@ -0,0 +1,28 @@
+﻿using GetHashCode;
+
+int valor1 = 10;
+int valor2 = 20;
+
+//Comparativo entre tipos primitivos
+Class1 comparador = new();
+Console.WriteLine($"Resultado da comparação entre valor1 e valor2: {comparador.Comparar(valor1, valor2)}");
+
+//Comparativo entre tipos definidos pelo usuário 
+var pessoa1 = new Pessoa(1, "Ana");
+var pessoa2 = new Pessoa(1, "Ana");
+Console.WriteLine($"Resultado da comparação de pessoa1 e pessoa2: {pessoa1.Equals(pessoa2)}");
+//Saída : false
+
+pessoa1 = pessoa2;
+Console.WriteLine($"Resultado da comparação de pessoa1 e pessoa2: {pessoa1.Equals(pessoa2)}");
+//Saíde : true (apontam para a mesma referência)
+
+var pessoa3 = new Pessoa(1, "Matheus");
+Console.WriteLine($"Resultado da comparação de pessoa1 e pessoa3: {pessoa1.Equals(pessoa3)}");
+//Saída : True (pois ambos possuem o mesmo ID)
+
+var pessoa4 = new Pessoa(2, "Matheus");
+Console.WriteLine($"Resultado da comparação de pessoa3 e pessoa4: {pessoa3.Equals(pessoa4)}");
+//Saída : False (pois ambos possuem diferentes ID)
+
+Console.Read();
