51dca0d54d648092c26da458914fc994fdabeb74
Matheus Sena <matheussena02@gmail.com>
2025-09-09 06:57:01 -0300

    feat: Adiciona o projeto ExercicioDictionary à solução principal do projeto com os arquivos de código que solucionam os exercícios propostos

---
+++ ExercicioDictionary/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Class1
+{
+    public class Aluno
+    {
+        private double Nota;
+        private string? Nome;
+
+        public Aluno(string? nome, int nota)
+        {
+            Nome = nome;
+            Nota = nota;
+        }
+
+        public void ExibeNotaAluno()
+        {
+            Console.WriteLine($"{Nome} - {Nota}");
+        }
+
+        public static int BuscaAluno(Dictionary<int, Aluno> dict, string? nome)
+        {
+            foreach(var par in  dict)
+            {
+                if(par.Value.Nome == nome)
+                {
+                    return par.Key;
+                }
+                
+            }
+            return -1;
+        }
+    }
+}
---
+++ ExercicioDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using Class1;
+
+Dictionary<int, Aluno> listaAlunoNota = new Dictionary<int, Aluno>()
+{
+    {1, new Aluno("Maria", 7)},
+    {2, new Aluno("Eric", 8)},
+    {3, new Aluno("Ana", 9)},
+    {4, new Aluno("Alex", 6)},
+    {5,  new Aluno("Diná", 5)}
+};
+
+foreach(var item in listaAlunoNota)
+{
+    item.Value.ExibeNotaAluno();
+}
+
+Console.WriteLine(Aluno.BuscaAluno(listaAlunoNota, "Maria"));
+
+
+
+Console.Read();
