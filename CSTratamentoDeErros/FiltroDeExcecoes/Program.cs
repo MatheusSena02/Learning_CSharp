42ed2a485a865f4430e42c52baff036fed0aab35
Matheus Sena <matheus.sena@renova.net.br>
2025-09-15 10:34:02 -0300

    feat: Adiciona o projeto ExercicioTratamentoDeErros à solução do projeto

--- CSTratamentoDeErros/FiltroDeExcecoes/Program.cs
+++ CSTratamentoDeErros/FiltroDeExcecoes/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    Console.Write("Informe o numerador: ");
+    int valor1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Informe o denominador: ");
+    int valor2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Resultado da divisão: {valor1 / valor2}");
+
+    Console.Read();
+}
+catch (Exception ex) when (ex.Message.Contains("format")) //Lança a exceção se a mensagem de exceção conter a palavra "format"
+{
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine("Erro de formatação");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
