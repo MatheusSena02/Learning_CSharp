93c3bf07d47135b62d85616d982b72875a781aef
Matheus Sena <matheus.sena@renova.net.br>
2025-09-15 09:52:05 -0300

    feat: Incrementa o README com informações acerca do lançamento de exceções e o uso do operador throw e Incrementa os arquivos de código com demonstração do uso do operador throw

--- CSTratamentoDeErros/LancamentoDeExcecoesThrow/Program.cs
+++ CSTratamentoDeErros/LancamentoDeExcecoesThrow/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿A.ProcessarA();
+Console.Read();
+class A
+{
+    public static void ProcessarA()
+    {
+        try
+        {
+            B.ProcessarB();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Tratando a exceção na classe A");
+        }
+        
+    }
+}
+class B
+{
+    public static void ProcessarB()
+    {
+        try
+        {
+            C.ProcessarC();
+        }
+        catch (Exception)
+        {
+            {
+                throw;  //Pula para a próxima iteração da stack trace
+                Console.WriteLine("Tratando a exceção na classe B");
+            }
+
+        }
+    }
+}
+
+class C
+{
+    public static void ProcessarC()
+    {
+        throw new NotImplementedException("Método não implementado");
+    }
+}
