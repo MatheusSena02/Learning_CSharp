aa58c7a0e059f9eb660c39bcd438d4a11370040b
Matheus Sena <matheus.sena@renova.net.br>
2025-09-16 08:12:45 -0300

    feat: Incrementa o projeto ExcecoesPersonalizadas com a classe SaldoInsuficienteException e o arquivo Program.cs com arquivos de código demonstrativo do uso de Exceções Personalizadas

---
+++ CSTratamentoDeErros/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,51 @@
+﻿using ExcecoesPersonalizadas;
+
+try
+{
+    Conta conta1 = new Conta(100, "João", 100m);
+    Console.WriteLine(conta1.ToString());
+    Console.WriteLine(conta1.Depositar(100));
+    Console.WriteLine(conta1.Sacar(1000));
+}catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+Console.Read();
+
+public class Conta
+{
+    public int Numero {  get; set; }
+    public string? Titular { get; set; }
+    public decimal Saldo { get; set; }
+
+    public Conta(int numero, string? titular, decimal saldo)
+    {
+        Numero = numero;
+        Titular = titular;
+        Saldo = saldo;
+    }
+
+    public decimal Depositar(decimal valor)
+    {
+        Saldo += valor;
+        return Saldo;
+    }
+
+    public decimal Sacar(decimal valor)
+    {
+        if(Saldo < valor)
+        {
+            throw new SaldoInsuficienteException(valor, Saldo);
+        }
+        Saldo -= valor;
+        return Saldo;
+    }
+
+    public override string ToString()   //O método ToString() é um método virtual, ou seja, permite sobrescrever o método
+    {
+        return $"Conta: {Numero} : {Titular} - Saldo = {Saldo}";
+    }
+
+}
---
+++ CSTratamentoDeErros/ExcecoesPersonalizadas/SaldoInsuficienteException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExcecoesPersonalizadas
+{
+    public class SaldoInsuficienteException: Exception
+    {
+        public SaldoInsuficienteException() { }     //Primeiro construtor comum
+
+        public SaldoInsuficienteException(string? message) : base(message) { }  //Segundo construtor comum
+
+        public SaldoInsuficienteException(string? message, Exception? innerException) : base(message, innerException) { }   //Terceiro construtor comum
+
+        public SaldoInsuficienteException(decimal saque, decimal saldo) : base($"\nException: Valor do saque R${saque - saldo} superior ao saldo") { }
+
+        public override string Message  //Message também é um método virtual da classe Exception. Logo, é possível sobrescrever
+        {
+            get
+            {
+                return "Não há saldo suficiente para sacar";
+            }
+        }
+    }
+}
