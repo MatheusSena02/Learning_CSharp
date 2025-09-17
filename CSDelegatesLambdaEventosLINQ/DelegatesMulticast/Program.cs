ca07324fe25ddf02a7fb83c02283ee09689d0236
Matheus Sena <matheus.sena@renova.net.br>
2025-09-17 08:58:48 -0300

    feat: Incrementa o arquivo Program.cs com código demonstrativo do uso de MulticastDelegates

--- CSDelegatesLambdaEventosLINQ/DelegatesMulticast/Program.cs
+++ CSDelegatesLambdaEventosLINQ/DelegatesMulticast/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Impressao impressaoMensagem = (string message) => Metodo1(message);
+impressaoMensagem += Metodo2;
+
+impressaoMensagem.Invoke("Hello World!");
+
+var metodoB = impressaoMensagem.GetInvocationList()[1];
+metodoB.DynamicInvoke("Mensagem diferente"); 
+
+Console.Read();
+
+static void Metodo1(string message)
+{
+    Console.WriteLine(message);
+}
+
+static void Metodo2(string message)
+{
+    Console.WriteLine($"Messagem 2 : {message}");
+}
+
+public delegate void Impressao(string messagem);
