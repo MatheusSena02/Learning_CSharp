b3f50904cdd7b2c3930bc3c446098afb2d895dec
Matheus Sena <matheus.sena@renova.net.br>
2025-09-19 09:23:51 -0300

    feat: Incrementa o arquivo Program.cs do projeto DelgateAction com códigos que demonstram o uso do Delegate Action

--- CSDelegatesLambdaEventosLINQ/DelegateAction/Program.cs
+++ CSDelegatesLambdaEventosLINQ/DelegateAction/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Informe um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Action<int> dobra = x => Console.Write($"Resultado: {x* 2}\n");
+//delegate void Action <int T> (T obj)
+
+dobra(numero);
+
+Action dobraPadrao = Duplicar;
+dobraPadrao.Invoke(5);
+
+Console.Read();
+
+static void Duplicar(int num)
+{
+    int resultado = num * 2;
+    Console.Write($"Resultado: {resultado}");
+}
+
+public delegate void Action(int numero);
