24700253916de247af28bfb693b43bc1ca70b997
Matheus Sena <matheus.sena@renova.net.br>
2025-09-19 08:56:47 -0300

    feat: Incrementa o arquivo Program.cs do projeto DelegatePredicate com códigos que demonstram o uso do Delegate Predicate

--- CSDelegatesLambdaEventosLINQ/DelegatePredicate/Program.cs
+++ CSDelegatesLambdaEventosLINQ/DelegatePredicate/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Informe um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+DelegatePar delegatePar = VerificaPar;
+
+Predicate <int> delegatePredicatePar = VerificaPar;
+
+//Utilizando Predicate e Expressão Lambda
+Predicate <int> predicatePar = x => x % 2 == 0;
+
+if (predicatePar(numero)) { 
+    Console.WriteLine($"O número {numero} é PAR");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ÍMPAR");
+}
+
+Console.Read();
+
+    static bool VerificaPar(int x)
+    {
+        if (x % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+//Método Pãdrão sem o Predicate
+public delegate bool DelegatePar(int x);
