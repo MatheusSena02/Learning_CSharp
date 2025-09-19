e69b82cb83f3f7b7da8d77114f26fdb66f088f2b
Matheus Sena <matheus.sena@renova.net.br>
2025-09-19 09:51:17 -0300

    feat: Incrementa o arquivo Program.cs do projeto DelegateFunc com código que demonstra o uso do DelegateFunc

--- CSDelegatesLambdaEventosLINQ/DelegateFunc/Program.cs
+++ CSDelegatesLambdaEventosLINQ/DelegateFunc/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Digite um número: ");
+double numero = Convert.ToInt32(Console.ReadLine());
+
+Func<double, double> raizQuadrada = x => Math.Sqrt(x);
+
+double resultado = raizQuadrada(numero);
+
+Console.WriteLine($"\nA raiz quadrada do {numero} é {resultado}");
+
+Console.Read();
