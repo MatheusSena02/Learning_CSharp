8388850215841bc09a10b7dcdc2187693769cb08
Matheus Sena <matheus.sena@renova.net.br>
2025-09-22 08:41:24 -0300

    feat: Incrementa o arquivo Program.cs do projeto EventHandler com o código adaptado utilizando o delegate pré=definido EventHandler

--- CSDelegatesLambdaEventosLINQ/EventHandler/Program.cs
+++ CSDelegatesLambdaEventosLINQ/EventHandler/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var pedido = new Pedido();
+
+//Inscreve os assinantes no evento, ou seja, aquilo que será disparado quando o evento for chamado
+pedido.OnCriarPedido += Email.Send;
+pedido.OnCriarPedido += SMS.Send;
+
+pedido.CriarPedido();
+
+Console.Read();
+
+//Não necessita declarar um delegate
+
+public class Pedido
+{
+    public event EventHandler? OnCriarPedido;
+
+    public void CriarPedido()
+    {
+        Console.WriteLine("\nPedido criado!!!");
+        if (OnCriarPedido != null)   //Verifica se o evento foi criado com sucesso
+        {
+            // Dispara (invoca) o evento OnCriarPedido, passando o objeto atual (this) como remetente
+            // e EventArgs.Empty como argumento padrão (sem dados adicionais).
+            // Isso faz com que todos os métodos inscritos no evento sejam executados.
+            // No contexto deste código, os métodos Email.Send e SMS.Send serão chamados.
+            OnCriarPedido(this, EventArgs.Empty);    //Invoca o evento   
+        }
+    }
+}
+
+public class SMS
+{
+    // Método estático responsável por enviar um SMS quando o evento é disparado.
+    // Parâmetros:
+    //   sender: objeto que disparou o evento (neste caso, a instância de Pedido)
+    //   e: argumentos do evento (EventArgs.Empty, pois não há dados adicionais)
+    public static void Send(object? sender, EventArgs e)
+    {
+        Console.WriteLine("\nEnviando um SMS");
+    }
+}
+
+public class Email
+{
+    public static void Send(object? sender, EventArgs e)
+    {
+        Console.WriteLine("\nEnviando um email");
+    }
+
+}
