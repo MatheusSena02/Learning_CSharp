284fc0026f717c958eb412a690fece5b548f040d
Matheus Sena <matheus.sena@renova.net.br>
2025-09-22 08:19:47 -0300

    feat: Incrementa o arquivo Program.cs do projeto Eventos com o código que demonstra o uso de Eventos na linguagem C#

--- CSDelegatesLambdaEventosLINQ/Eventos/Program.cs
+++ CSDelegatesLambdaEventosLINQ/Eventos/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var pedido = new Pedido();
+
+//Inscreve os assinantes no evento, ou seja, aquilo que será disparado quando o evento for chamado
+pedido.OnCriarPedido += Email.Send;
+pedido.OnCriarPedido += SMS.Send;
+
+pedido.CriarPedido();
+
+Console.Read();
+
+// Tornar o delegate público para corresponder à acessibilidade do evento
+public delegate void PedidoEventHandler();
+
+public class Pedido
+{
+    public event PedidoEventHandler? OnCriarPedido;
+
+    public void CriarPedido()
+    {
+        Console.WriteLine("\nPedido criado!!!");
+        if(OnCriarPedido != null)   //Verifica se o evento foi criado com sucesso
+        {
+            OnCriarPedido();    //Invoca o evento   
+        }
+    }
+}
+
+public class SMS
+{
+    public static void Send()
+    {
+        Console.WriteLine("\nEnviando um SMS");
+    }
+}
+
+public class Email
+{
+    public static void Send()
+    {
+        Console.WriteLine("\nEnviando um email");
+    }
+    
+}
