515f592998d36689fd53191ce1c19152cd176a2d
Matheus Sena <matheus.sena@renova.net.br>
2025-09-24 10:48:16 -0300

    feat: Incrementa o arquivo Program.cs do projeto DirectoryInfo com código demonstrativo acerca do uso da classe DirectoryInfo e seus métodos

--- CSArquivosDiretoriosStreams/DirectoryInfo/Program.cs
+++ CSArquivosDiretoriosStreams/DirectoryInfo/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var caminhoDiretorio = @"C:\Users\matheus.sena\Documents";
+
+var dirOrigem = new DirectoryInfo(caminhoDiretorio);
+
+Console.WriteLine($"Nome do diretório: {dirOrigem.Name}");
+Console.WriteLine($"Caminho do diretório: {dirOrigem.FullName}");
+Console.WriteLine($"Data de criação do diretório: {dirOrigem.CreationTime}");
+Console.WriteLine($"Último acesso do diretório: {dirOrigem.LastAccessTime}");
+Console.WriteLine($"Última modificação do diretório: {dirOrigem.LastWriteTime}");
+Console.WriteLine($"Atributos: {dirOrigem.Attributes}");
+
+//Criação de um novo diretório 
+
+var novoDiretorio = new DirectoryInfo(@"C:\Users\matheus.sena\Documents\MeuDiretorio");
+
+try
+{
+    if(!novoDiretorio.Exists)
+    {
+        novoDiretorio.Create();
+    }
+    else
+    {
+        throw new Exception("Diretório já existente");
+    }
+}catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//Deleção do Diretório
+try
+{
+    if (!novoDiretorio.Exists)
+    {
+        throw new Exception("Diretório não existente");
+    }
+    else
+    {
+        novoDiretorio.Delete();
+        Console.WriteLine("Diretório excluído com sucesso");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//Obtendo subdiretórios
+Console.WriteLine("\nObtendo os subdiretórios do diretórios: ");
+try
+{
+    foreach(var sub in dirOrigem.GetDirectories())
+    {
+        Console.WriteLine(sub.Name);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+//Obtendo os arquivos
+Console.WriteLine("\nObtendo os arquivos do diretórios: ");
+try
+{
+    foreach (var arq in dirOrigem.GetFiles())
+    {
+        Console.WriteLine(arq.Name);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
