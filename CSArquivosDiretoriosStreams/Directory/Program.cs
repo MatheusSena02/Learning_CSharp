c033fb1b0c68b5498a52599f377b173e5a49c623
Matheus Sena <matheus.sena@renova.net.br>
2025-09-24 10:16:39 -0300

    feat: Incrementa o arquivo Program.cs do projeto Directory com código que demonstra o uso da classe Directory e seus métodos

--- CSArquivosDiretoriosStreams/Directory/Program.cs
+++ CSArquivosDiretoriosStreams/Directory/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿var caminhoDiretorio = @"C:\Users\matheus.sena\Documents\diretorio_learningcsharp";
+
+
+//Criação de repositório
+
+try     //Recomenda-se utilizar o bloco try-catch ao criar diretórios
+{
+    if (!Directory.Exists(caminhoDiretorio))
+    {
+        Directory.CreateDirectory(caminhoDiretorio);
+        Console.WriteLine("Diretório criado com sucesso");
+    }
+    else
+    {
+        throw new Exception("Diretório já existente");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message); 
+}
+
+//Deleção do diretório
+
+try    
+{
+    if (Directory.Exists(caminhoDiretorio))
+    {
+        Directory.Delete(caminhoDiretorio);
+        Console.WriteLine("Diretório deletado com sucesso");
+    }
+    else
+    {
+        throw new Exception("Diretório não existente");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+Console.Read();
