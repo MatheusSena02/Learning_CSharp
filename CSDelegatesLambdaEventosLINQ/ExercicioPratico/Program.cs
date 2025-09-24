c7f96ae467f396cbfd21b5a47091abc6cd251cc0
Matheus Sena <matheus.sena@renova.net.br>
2025-09-24 08:19:02 -0300

    feat: Adiciona o projeto ExercicioPratico com os arquivos de código que contém a resolução do exercício proposto

---
+++ CSDelegatesLambdaEventosLINQ/ExercicioPratico/Program.cs
@@ -0,0 +1,11 @@
+﻿MensagemBoasVindas impressao = delegate ()
+{
+    Console.WriteLine("Seja bem-vindo(a)");
+};
+impressao += () => Console.WriteLine("Welcome");
+
+impressao.Invoke();
+
+Console.Read();
+
+public delegate void MensagemBoasVindas();
